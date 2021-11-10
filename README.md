b147f4c7dd20668838deabd92ebc42b0d673a695
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2021-11-10 23:11:57 +0200

    functionality almost complete

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # library-
+This is the library Project for The odin Project 
+started on 10/11/2021
--- script.js
+++ script.js
@@ -1,42 +1,85 @@
-/*let Book = function(title, author, pages , read){ 
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read= read
-}
-Book.prototype.info =function(){ 
-    console.log(`${this.title} by ${this.author}, ${this.pages} pages, Is Read ${this.read}`);
-} 
-
-let Hoob = Book('The Hobbit ',"J.J.R Tolkien",295,false)
-Hoob.info()
-*/
-let myLibrary = [];
+// varibles i will need
+// let myLibrary = [];
+let title = document.querySelector('.title');
+let author = document.querySelector('.author');
+let pages = document.querySelector('.pages');
+let isread = document.querySelector('.isread')
+let addbook = document.querySelector('.add')
+// Book constructor
 function Book(title,author,pages,read){ 
-    this.title = title 
-    this.author = author 
-    this.pages = pages 
-    this.read  = read
+    this.title = title ;
+    this.author = author ;
+    this.pages = pages ;
+    this.read  = read;
 }
-function addBookToLibrary(){ 
-
+// add book to the array
+function addBookToLibrary(book){ 
+    myLibrary.push(book)
 }
-myLibrary = [
-    {title:'first book for you ' , author:'ahmed maher' , pages:200 , read:true },
-    {title:'Book rrrrrrrrrro ' , author:'maher' , pages:100 , read: false }
+// when add button is clicked
+addbook.addEventListener('click',function(){ 
+    let book =new Book(title.value,author.value,parseInt(pages.value),isread.checked);
+    addBookToLibrary(book)
+    title.value =  author.value = pages.value = '';
+    isread.checked = false;
+})
+// temp library for testing the functions
+let myLibrary = [
+    {title: 'aaaa',
+        author: 'aaaaaaaaaaaaaaaaa',
+        pages:200,
+        isread: false },
+    {title: 'bbb',
+        author: 'bbbbbbbbbbbb',
+        pages:100,
+        isread: true}
 ]
-let table = document.querySelector('table')
-for (let index in myLibrary){ 
-    let row  = document.createElement('tr')
-    let book = myLibrary[index]
-    for(let atr in book){ 
-        console.log(book[atr])
-        let cell = document.createElement('td')
-        cell.textContent = book[atr];
-        row.appendChild(cell)
+
+
+let showbord = document.querySelector('.showbord')
+// display the librar each book in a card 
+function display(library){ 
+    for(let index in library){
+        let card = document.createElement("div");
+        card.className = 'card';
+        showbord.appendChild(card)
+        let book = library[index]
+        for(let info in book){
+            let row = document.createElement('div')
+            row.className='row'
+            row.textContent= book[info]
+            card.appendChild(row)
+        }
+        // adding the remove from library button and associating it with the book
+        let remover = document.createElement('button');
+        remover.setAttribute('data-index',`${index}`);
+        remover.className='delete-book'
+        remover.textContent = 'Delete';
+        card.appendChild(remover);
+        // adding a button to change the read status for the book
+        let readingStatus = document.createElement('button');
+        readingStatus.setAttribute('data-index',`${index}`);
+        readingStatus.className='reading-book';
+        readingStatus.textContent='Read It'
+        card.appendChild(readingStatus)
     }
-    table.appendChild(row)
 }
+// ivoking the display function
+display(myLibrary);
+let deletebook = Array.from(document.querySelectorAll('.delete-book'))
+deletebook.map(delbtn=>{ 
+    delbtn.addEventListener('click',function(e){
+        let theindex = e.target.dataset.index
+        myLibrary = myLibrary.filter((book,indexofbook)=> {
+            return indexofbook != theindex;
+        })
+        display(myLibrary)
+    })
+})
 
 
-
+/*    let book = Object.create(Book);
+    book.title = title.value;
+    book.author = author.value;
+    book.pages = parseInt(pages.value);
+    book.isread = isread.checked*/
