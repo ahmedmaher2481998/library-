7309912c4d244fa241686fec9f8c038a3b503f32
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2021-11-10 23:12:29 +0200

    adding israd to prototype

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ function Book(title,author,pages,read){
     this.pages = pages ;
     this.read  = read;
 }
+Book.prototype.isread{ 
+    
+}
 // add book to the array
 function addBookToLibrary(book){ 
     myLibrary.push(book)
