4d0d753cccbce533f3bf4cca8702a0bdfbdf99db
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2021-11-13 20:08:26 +0200

    strugling

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 // declaring the library
 /*
 issues to fix 
---local storage working 
+--local storage working //done
 --extera words with the info the user provides
 --delete button working 
 --read button working 
 -- wraped titles and text 
-
  */
-let myLibrary = JSON.parse(localStorage.getItem("library"));
+
+let myLibrary = get();
 if(!myLibrary || myLibrary=='null'){ 
     myLibrary= [];
-    localStorage.setItem("library", JSON.stringify(myLibrary))
+    set();
 }
  
 // varibles i will need
+let displayed;
 let title = document.querySelector('.title');
 let author = document.querySelector('.author');
 let pages = document.querySelector('.pages');
 let isread = document.querySelector('.isread')
 let addbook = document.querySelector('.add')
 let showbord = document.querySelector('.showbord')
-let readbook = document.querySelectorAll(".reading-book")
 // ivoking the display function
 display(myLibrary);
+// when add button is clicked
 addbook.addEventListener('click',function(){
     
     if(!title.value || !author.value || pages.value < 0 ||!pages.value){
@@ -32,7 +33,7 @@ addbook.addEventListener('click',function(){
     else { 
             let book =new Book(title.value,author.value,parseInt(pages.value),isread.checked);
             addBookToLibrary(book)
-            localStorage.setItem("library",JSON.stringify(myLibrary));
+            set();
             title.value =  author.value = pages.value = '';
             isread.checked = false;
     }
@@ -42,6 +43,12 @@ addbook.addEventListener('click',function(){
 
 // display the librar each book in a card 
 function display(library){ 
+    if(displayed){ 
+        resetdisplay
+    }
+    else{ 
+        displayed = true
+    }
     for(let index in library){
         let card = document.createElement("div");
         card.className = 'card';
@@ -74,14 +81,10 @@ function display(library){
 }
 // reset display 
 function resetdisplay(){ 
-    // let cards = Array.from(document.querySelectorAll('.card'));
-    // cards.map(card =>{ 
-    //     card.parentElement.removeChild()
-    // })
     showbord.innerHTML='';
-
+    displayed=false;
 }
-// deleteing a book from the library 
+// deleteing a book from the library
 let deletebook = Array.from(document.querySelectorAll('.delete-book'))
 deletebook.map(delbtn=>{ 
     delbtn.addEventListener('click',function(e){
@@ -91,28 +94,25 @@ deletebook.map(delbtn=>{
         })
         resetdisplay();
         display(myLibrary)
-        localStorage.setItem("library",JSON.stringify(myLibrary))
+        set();
     })
 })
-// read book change status 
-Array.from(readbook).map(readbutton=>{ 
-    readbutton.addEventListener('click',function(e){ 
-        let theindex = e.target.dataset.index;
-        myLibrary = myLibrary.map((book,bookindex)=>{
-            if(bookindex == theindex){ 
-                book.changeread();
-                display(myLibrary)
-            } 
+// read / unread button
+let readbook = Array.from(document.querySelectorAll('.reading-book'))
+readbook.map(readbookbtn=>{ 
+    readbookbtn.addEventListener('click',function(e){ 
+        myLibrary.map((book,index)=>{ 
+            if(index == e.target.dataset.index){
+                book.read = !(book.read)
+                set();
+                resetdisplay();
+                display(get())
+
+            }
         })
-    });
-});
-// changing read status 
-let readitbuttons = Array.from(document.querySelectorAll('.reading-book'));
-readitbuttons.map(readitbutton=>{ 
-    addEventListener('clikc',function(e){ 
-        console.log(e.target.dataset.index)
     })
 })
+
 
 // Book constructor
 let Book = function(title,author,pages,read){ 
@@ -121,12 +121,27 @@ let Book = function(title,author,pages,read){
     this.pages = pages ;
     this.read  = read;
 }
+/*
+Book.prototype.readit = function(){ 
+        let now = this.read
+        this.read = !now
+    }
+/*
 Book.prototype.changeread= function(){
     let current = this.read ;
     this.read = !current;
 }
+*/
 // add book to the array
 function addBookToLibrary(book){ 
     myLibrary.push(book)
 }
-// when add button is clicked
+// get the myLibrary array from the localStorage
+function get(){ 
+    return JSON.parse(localStorage.getItem("library"));
+} 
+// modify the myLibrary array atthe localStorage
+
+function set(){ 
+    localStorage.setItem("library", JSON.stringify(myLibrary))
+}
